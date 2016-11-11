feb9e7396c16f76389cd648df97970e998c14622
pixels-bytes <adam@pixelsandbytes.co>
2016-11-11 18:44:13 +0100

    Pug templating

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+/*jslint node: true, esversion: 6 */
+"use strict";
+
+
+/**
+ * CONTINUOUS DEPLOYMENT
+ * @version 1.0.0
+ * @author Pixels & Bytes
+ *
+ *         Getting Contentful — Github — Travis-CI — Heroku all talking to eachother
+ *         Contentful: CaaS
+ *         GitHub: Repositories & Pages
+ *         Travis-CI: The build
+ *         Heroku: Handling Contentful updates
+ *
+ * @requires pug
+ */
+
+
+
+// THE REQUIREMENTS
+const pug = require('pug');
+
+
+
+const index = pug.compileFile('index.pug');
+const post = {
+  title: 'Contentful — GitHub — Github Pages — Heroku — Travis-CI',
+  content: 'Getting Contentful, Github, Github pages, Heroku, and Travis-CI all talking to eachother with webhooks.'
+};
+
+console.log(index(post));
