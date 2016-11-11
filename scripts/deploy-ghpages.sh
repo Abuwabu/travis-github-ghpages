cd2fe18e6dfd126c5972c9cb5fa9227e67e1d7ff
pixels-bytes <adam@pixelsandbytes.co>
2016-11-12 00:09:55 +0100

    branches only develop

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -6,4 +6,4 @@ git config user.email "adam@pixelsandbytes.co"
 git add .
 git commit -m "Deployed to Github Pages"
 git remote add origin git@github.com:pixels-bytes/contentful-travis-github.git
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" develop:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
