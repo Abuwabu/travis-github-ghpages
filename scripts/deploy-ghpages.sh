ea0da017f8843bbe7ba054d902151e611d2d9c6b
pixels-bytes <adam@pixelsandbytes.co>
2016-11-11 23:25:39 +0100

    Removed brackets and pointed npm run deploy to the right place

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-(
 cd build
 git init
 git config user.name "pixels-bytes"
@@ -8,4 +7,3 @@ git add .
 git commit -m "Deployed to Github Pages"
 git remote add origin git@github.com:pixels-bytes/contentful-travis-github.git
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
-)
