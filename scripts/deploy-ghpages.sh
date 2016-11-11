abb242d6491fc6b417a60510cea4681311bd6cab
pixels-bytes <adam@pixelsandbytes.co>
2016-11-11 23:18:12 +0100

    Brackets in deploy.sh

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+(
 cd build
 git init
 git config user.name "pixels-bytes"
@@ -7,3 +8,4 @@ git add .
 git commit -m "Deployed to Github Pages"
 git remote add origin git@github.com:pixels-bytes/contentful-travis-github.git
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+)
